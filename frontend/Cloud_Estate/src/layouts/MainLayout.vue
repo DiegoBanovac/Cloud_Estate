37b75de1769211e544c34c8c522d296edc5bde3e
DiegoBanovac <diego.banovac10@gmail.com>
2024-11-06 19:15:59 +0100

    Prvi commit sa izradenim quasar projektom te dodanom stranicom naziva Kupnja nekretnina

---
+++ frontend/Cloud_Estate/src/layouts/MainLayout.vue
@@ -0,0 +1,76 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="toggleLeftDrawer"
+        />
+
+        <q-toolbar-title>
+          Cloud Estate
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+    >
+      <q-list>
+        <q-item-label
+          header
+        >
+          Essential Links
+        </q-item-label>
+
+        <EssentialLink
+          v-for="link in linksList"
+          :key="link.title"
+          v-bind="link"
+        />
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import EssentialLink from 'components/EssentialLink.vue'
+
+defineOptions({
+  name: 'MainLayout'
+})
+
+const linksList = [
+{
+    title: 'Naslovnica',
+    caption: 'Početna stranica',
+    icon: 'house',
+    link: '#/'
+  },
+  {
+    title: 'Kupi nekretninu',
+    caption: 'Dostupne nekretnine za kupnju',
+    icon: 'home_work',
+    link: '#/kupnja_nekretnina'
+  },
+]
+
+const leftDrawerOpen = ref(false)
+
+function toggleLeftDrawer () {
+  leftDrawerOpen.value = !leftDrawerOpen.value
+}
+</script>
